6609de19f45f51e391e9d028b7a5915456f2a11b
Gabriel Duke <gabeduke@gmail.com>
2017-07-08 23:21:10 -0400

    Update level.py

--- level.py
+++ level.py
@@ -2,17 +2,25 @@
 import xml.etree.ElementTree as ET
 import requests
 
-# Gather XML Data
-link = "http://water.weather.gov/ahps2/hydrograph_to_xml.php?gage=rmdv2&output=xml"
-xmlString = requests.get(link).text
 
-# Parse XML Data
-root = ET.fromstring(xmlString)
+def main():
+  # Gather XML Data
+  link = "http://water.weather.gov/ahps2/hydrograph_to_xml.php?gage=rmdv2&output=xml"
+  xmlString = requests.get(link).text
 
-# Parse Latest Reading
-latest = root.find("./observed/datum")
+  # Parse XML Data
+  root = ET.fromstring(xmlString)
+
+  # Parse Latest Reading
+  latest = root.find("./observed/datum")
 
 
-# Print latest reading
-print("River Level is: " + latest.find("primary").text)
-print("http://water.weather.gov/resources/hydrographs/rmdv2_hg.png")
+  # Print latest reading
+  print("River Level is: " + latest.find("primary").text)
+  print("http://water.weather.gov/resources/hydrographs/rmdv2_hg.png")
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
+    
